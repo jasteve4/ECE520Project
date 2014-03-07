7346b589b129bd91f0b740ae230d4a557bb6ae27
audiocircuit <scott891@gmail.com>
2014-03-07 15:08:53 -0500

    The test bench properly reads in files now

--- Input_Pipeline/input_pipeline_tb.v
+++ Input_Pipeline/input_pipeline_tb.v
@@ -1,11 +1,26 @@
 module input_pipeline_tb();
 
+reg clock, WE;
+reg [15:0] WriteAddress, ReadAddress;
+reg [127:0] WriteBus;
+wire [127:0] ReadBus;
+
 reg [127:0] input_data [0:65535]; 
 reg [127:0] memory [0:65535]; 
 integer i;
 
+always #5 clock = ~clock;
+
 initial begin
-  $readmemh("../Hex_Files/input_small_hex.txt",input_data);
+  clock = 0;
+  WE = 1;
+  $readmemh("input_small_hex.txt",m1.Register);
+  #500
+  $writememh("output.txt",m1.Register);
+  $finish;
 end
+
+sram_1R1W m1(.clock(clock), .WE(WE), .WriteAddress(WriteAddress), .ReadAddress(ReadAddress), 
+        .WriteBus(WriteBus), .ReadBus(ReadBus));
 
 endmodule
